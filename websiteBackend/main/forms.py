bb238c36eb8c7e4979f87cb944d943127ae5dbcb
logan-goebel <logan.goebel@ndsu.edu>
2025-05-05 13:27:38 -0500

    Add files via upload

--- websiteBackend/main/forms.py
+++ websiteBackend/main/forms.py
@@ -7,20 +7,27 @@ from .models import UserProfile
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     phone_number = forms.CharField(max_length=15, required=False)
-    address = forms.CharField( required=False)
+    address = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), required=False)
     
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
     
-    def save(self):
-        user = super().save()
-        # Try to get the profile, if it doesn't exist, create it
-        try:
-            user_profile = UserProfile.objects.get(user=user)
-        except UserProfile.DoesNotExist:
-            user_profile = UserProfile.objects.create(user=user)
-        # Additional logic for the user_profile
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        
+        if commit:
+            # Try to get the profile, if it doesn't exist, create it
+            try:
+                user_profile = UserProfile.objects.get(user=user)
+            except UserProfile.DoesNotExist:
+                user_profile = UserProfile.objects.create(user=user)
+            
+            # Save the profile fields from the form
+            user_profile.phone_number = self.cleaned_data.get('phone_number', '')
+            user_profile.address = self.cleaned_data.get('address', '')
+            user_profile.save()
+            
         return user
 
 
