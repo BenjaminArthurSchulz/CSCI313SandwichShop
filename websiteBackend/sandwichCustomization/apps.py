4993db3bb3033232b078fd634a76c086c67ed2f0
logan-goebel <logan.goebel@ndsu.edu>
2025-05-06 20:46:57 -0500

    sandwich customization

---
+++ websiteBackend/sandwichCustomization/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SandwichcustomizationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sandwichCustomization"
---
+++ websiteBackend/sandwichCustomization/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('customize/', views.SandwichCreateView.as_view(), name='customize_sandwich'),
+
+
+]
